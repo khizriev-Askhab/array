5d35ff5bacef69ece6eb2b407b8eb86d87e403ba
khizriev <askhab.askhabb.khizriev@mail.ru>
2023-01-08 17:18:32 +0300

    сделал

--- array.js
+++ array.js
@@ -1,75 +1,169 @@
-// 1.))  Дан массив let a = ['lorem', 123, true, 'ipsum'].  Вывести длинну массива.
 
-let a = ['lorem', 123, true, 'ipsum'];
-console.log(a.length);
+                                                  //forEach
 
-
-//2.))Для того же массива выведите первый элемент и последний элемент (с учетом того, что количество элементов может меняться, а ваш код должен всегда работать)
+// 1-задание )) Дан массив с числами [10, 2762, 0.42, 192, 3, 28, 1125, 2018, 34, 66, 100]. Вывести в консоль только нечетные числа. (Для этого вам необходимо использовать if и узнать оператор про оператор ‘остаток от деления’ - % 
 
 
-let b = ['lorem', 123, true, 'ipsum'];
-console.log(a[a.length -1]);
-console.log(a[0]);
-
-
-//3-4-5))Дан пустой массив let em = []; Заполните его 5-ю различными элементами. Используйте методы для работы с массивами
-
-
-
-let em = [1200000, 'html',true, 'javaScript', false]
-em.unshift('dskf',1244,'safsf');
-em.shift();
-em.pop();
-em.pop();
-  console.log(em);
-
-
-  // 6.))Используя цикл вывести числа от 0 до 1000 в консоль.
-
-for (let i = 0; i <= 1000; i++){
-console.log(i);
-}
-
-
-//7.))Дан массив [“html”, “css”, “js”, “react”] . Напишите код, который принимает с помощью prompt() любое значение и будет добавлять его в конец массива. 
-
-
-let t  = prompt (['html', 'css', 'js', 'react']);
-
-
-console.log(t);
-
-
-//8.))Используя цикл вывести все элементы предыдущего массива
-
-
-for (let i = 0; i < t.length; i++){
-    console.log(t[i]);
-};
-
-
-// 9.))Дан массив [10, 123, 13, 481, 1931]. Используя цикл вывести сумму элементов массива, т.е. 10+123+13… и т.д.
-
-let add = [10,123,13,481,1931];
-function arraySum(array){
-    let sum = 0;
-    for (let i = 0; i < array.length; i++){
-        sum += array[i];
+let array = [10, 2762, 0.42, 192, 3, 28, 1125, 2018, 34, 66, 100];
+array.forEach(function(a,b,c){
+    if ( a % 2){
+        console.log(a);
     }
-    console.log(sum);
-}
-arraySum(add);
 
 
-//Дан массив [“frontend”, [“js”, “html”, “css”], “react”, [“node js”, “mysql”], “php”] - как вы видите сами являются массивами: подумайте, как можно вывести элементы вложенного массива, к примеру “js”, “css”, “mysql”.
+});
+
+
+// 2-задание)) Дан массив со строчными элементами [ “lorem ipsum”, “javascript”, “php”, “css”, “react”, “git”, “html”, “mysql”]. Вывести в консоль элементы у которых кол-во символов не больше 4-х. 
+
+
+let arr = ['lorem ipsum', 'javascript', 'php', 'css', 'react', 'git', 'html', 'mysql'];
+
+arr.forEach(function(item,key){
+  if (item.length > 4 ){
+    console.log(item);
+  }
+
+
+});
+
+console.log(arr);
 
 
 
 
 
 
-let index = ['frontend', ['js', 'html', 'css'], 'react', ['node js', 'mysql'], 'php'];
+
+                                                          
+                                                          //   filter
+
+
+
+
+
+//1.  Дан массив [1, -3, 5, 6, -7, 8, 9, -11]. Создайте новый массив в котором останутся только отрицательные элементы. 
+
+
+
+let number = [1, -3, 5, 6, -7, 8, 9, -11];
+let newNumber = number.filter(function(item,key){
+  return item < 0;
+});
+console.log(newNumber);
+
+
+
+
+
+
+
+
+
+
+// 2. Дан массив [10, 2762, 0.42, 192, 3, 28, 1125, 2018, 34, 66, 100]. 
+//Создать новый в котором будут только четные элементы этого массива. (Нужен оператор остаток от деления - %). 
+
+
+let numbers =[10, 2762, 0.42, 192, 3, 28, 1125, 2018, 34, 66, 100];
+let newNumbers = numbers.filter(function(item,key) {
+  if ( item % 2 == 0 ){
+    console.log(item);
+  }
+  
+});
+
+
+
+
+// 3. Дан массив [ “lorem ipsum”, “javascript”, “php”, “css”, “react”, “git”, “html”, “mysql”] 
+//создать новый массив состоящий только из элементов у которых длина строки не равна 3-м.
+
+
+
+let lorem =[ 'lorem ipsum', 'javascript', 'php', 'css', 'react', 'git', 'html', 'mysql'];
+let newLorem = lorem.filter(function(item,key){
+  if(item.length <= 3){
+    console.log(item);
+  } 
+  
+});
+console.log(lorem);
+console.log(newLorem);
+
+
+
+
+
+                                                
+                                                        //Map
+
+
+
+//Дан массив [5, 6, 7, 8, 9], сделайте из него массив состоящий из квадратов этих числе. (25, 36.. и тд)
+
+
+let math = [5, 6, 7, 8, 9];
+let pow = math.map(function(item, key,arr){
+  arr[key]=item*item;
+});
+console.log(math);
+
+
+
+
+//Дан массив с объектами [{а: 10, b: 5}, {а: 20, b: 22}, {a: 131, b:55}].
+// Создать новый массив элементами которого буду сумма свойств объекта. Должно получится [15, 42, 186]
+
+
+
+let  arre = [{a: 10, b: 5}, {a: 20, b: 22}, {a: 131, b:55}];
+let arres = arre.map(function(item,key,arr){
+  return item.a + item.b;
+
+  
+});
+console.log(arres);
+
+
+
+
+
+                                        //Reduce
+
+
+
+
+//Дан массив [-13, 0, 12, 1662, -0.32, -102, -2]. 
+//Посчитать сумму отрицательных элементов. (Нужно будет использовать filter и reduce)
+
+
+
+let ar =[-13, 0, 12, 1662, -0.32, -102, -2];
+let fil = ar.filter(function(item,key,ar){
+  return item < 0;
  
-    console.log(index[1][0]);
- console.log(index[1][2]);
- console.log(index[3][1]);
+      
+});
+let red = ar.reduce(function(sum, item, key, ar){
+  return sum + item;
+
+})
+
+console.log(red);
+
+
+
+
+//Да массив с объектами [ {x: 10, y: “lorem”}, {x: 21, y: “lorem”}, {x: -17, y: “lorem”}, {x: 156, y: “lorem”}];
+// посчитать сумму свойств х всех объектов.
+
+
+
+let rud = [ {x: 10, y: 'lorem'}, {x: 21, y: 'lorem'}, {x: -17, y: 'lorem'}, {x: 156, y: 'lorem'}];
+
+let dod = rud.reduce(function(sum, item, index, arr){
+  return sum + item.x
+}, 0);
+
+console.log(dod);
